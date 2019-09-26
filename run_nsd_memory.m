c140bc3475aad61fddc7f8da799ea3ac8c4594ef
Ian Charest (School of Psychology) <i.charest@bham.ac.uk>
2019-09-26 11:58:09 +0100

    added buttons and click tests

--- run_nsd_memory.m
+++ run_nsd_memory.m
@@ -84,14 +84,38 @@ monthTicks = monthTicks(2:end-1);
 shadowdata(:, :, 4) = alpha;
 
 shadowtex = Screen('MakeTexture', mainWindow, shadowdata);
-ms=100;
+ms_buttons=100;
 
 % setup the alpha blending
 Screen(mainWindow,'BlendFunction',GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+% response keys for the seen before confidence response
+code1 = KbName('1!'); % 1
+code2 = KbName('2@'); % 2
+code3 = KbName('3#'); % 3
+code4 = KbName('4$'); % 2
+code5 = KbName('5%'); % 2
+code6 = KbName('6^'); % 2
+
+% define the keys we want
+keys = zeros(1,256);
+keys(code1)=1;
+keys(code2)=1;
+keys(code3)=1;
+keys(code4)=1;
+keys(code5)=1;
+keys(code6)=1;
+
+
+%% LOOPING THROUGH TRIALS
+% create a queue for monitoring events.
+KbQueueCreate(-1,keys);
+
+instructions = defineInstructions();
 
 
 % trial loop
+
 for imageI = 1:nimages
     
     % define that image's texture
@@ -99,16 +123,23 @@ for imageI = 1:nimages
     
     %% part 1: old (responses from 1 to 6) or new
     
-    % draw the image
+    % initiate the kb listener
+    % start the keyboard reading queue
+    KbQueueStart; % start the queue
+    queuestart = GetSecs; % time stamp when the queue starts 
     
+    
+    % draw the image    
     Screen('DrawTexture', mainWindow, imagetex,[],centerImageRect);
-    Screen('Flip', mainWindow);
+    onset = Screen('Flip', mainWindow);
+    
+    
+    
     % confidence rating
     
     WaitSecs(1)
     
     % do your response collection
-    
     
     % this will need to be dynamically set depending on participant's response.
     old = true;
@@ -123,6 +154,60 @@ for imageI = 1:nimages
         %% part 2: (only if not new): how many times?
         % if old, then
         % how many repetitions
+        
+        buttonAreas = drawButtons(mainWindow, win_rect);
+        % add texture on top
+        Screen('DrawTexture', mainWindow, imagetex,[],topImageRect);
+        % Flip
+        Screen('Flip', mainWindow);
+        
+        % find the mouse
+        [a,b]=WindowCenter(mainWindow);
+        SetMouse(a,b,SN);
+        
+        % show the texture 
+        % Main mouse tracking loop
+        mxold=0;
+        myold=0;
+        
+        notYetClicked = true;
+        
+        while notYetClicked
+            % We wait at least 10 ms each loop-iteration so that we
+            % don't overload the system in realtime-priority:
+            WaitSecs(0.01);
+            
+            % We only redraw if mouse has been moved:
+            [mx, my, buttons]=GetMouse(SN);
+            if (mx~=mxold || my~=myold)
+                
+                % redraw the buttons    
+                buttonAreas = drawButtons(mainWindow, win_rect);
+                % add texture on top
+                Screen('DrawTexture', mainWindow, imagetex,[],topImageRect);
+                % Flip
+                Screen('Flip', mainWindow);
+                
+            end
+            mxold=mx;
+            myold=my;
+            
+            % Break out of loop on mouse click
+            if find(buttons)
+
+                % this stores the button that was clicked
+                button_clicked = whichButtonClicked(mx, my, buttonAreas);                
+                if button_clicked
+                    break;
+                end
+            end
+        end
+        % Flip
+        Screen('Flip', mainWindow);
+        WaitSecs(1);
+        
+        clear buttons
+        
         
         %% part 3: timeline
         % choose on a timeline when you saw the image
